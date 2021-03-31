55657079ee20123ec1b3984f8a488fdec83f9b9c
Yeshi Tsering Gangchen <yestsering@gmail.com>
2021-03-31 15:22:42 +0200

    signup update

--- frontend/src/views/Signup.jsx
+++ frontend/src/views/Signup.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { } from "react";
 import 'antd/dist/antd.css';
-import { Form, Input, Button, Upload } from 'antd';
-import { useHistory } from "react-router-dom";
-import "./signup.css"
+import { Form, Input, Button, Upload, message } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+
 
 
 const layout = {
@@ -21,94 +21,49 @@ const tailLayout = {
 };
 
 const Signup = () => {
-    let history = useHistory();
 
-    const [imageUrl, setImage] = useState('');
-
-    const [isValid, setIsValid] = useState(false);
-    const [message, setMessage] = useState('');
-    const emailRegex = /\S+@\S+\.\S+/;
-
-    const [isPasswordValid, setIsPasswordValid] = useState(false);
-    const [messagePW, setMessagePW] = useState('');
-    const passwordRegex=  /^[a-zA-Z0-9!@#$%^&*]{8,}$/;
+    const props = {
+        name: 'file',
+        action: 'https://localhost:8000/public/profileimage/:id',
+        headers: {
+          authorization: 'authorization-text',
+        },
+        onChange(info) {
+          if (info.file.status !== 'uploading') {
+            console.log(info.file, info.fileList);
+          }
+          if (info.file.status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully`);
+          } else if (info.file.status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+          }
+        },
+        progress: {
+          strokeColor: {
+            '0%': '#108ee9',
+            '100%': '#87d068',
+          },
+          strokeWidth: 3,
+          format: percent => `${parseFloat(percent.toFixed(2))}%`,
+        },
+      };
 
     const onFinish = async (signupData) => {
         try {
             console.clear();
             console.log('Success:', signupData);
-            fetch("http://localhost:8000/signup", {
+            fetch("http://localhost:8000/singup", {
                 method: "POST",
                 headers: {
                     "Content-type": "application/json"
                 },
                 body: JSON.stringify(signupData)
-            });
-
-            history.push('/profile')
-
+            })
         } catch (err) {
             console.error(err)
         }
     };
 
-    const uploadButton = (
-        <div>
-            <div style={{ marginTop: 8 }}>Upload</div>
-        </div>
-    );
-
-    function getBase64(img, callback) {
-        const reader = new FileReader();
-        reader.addEventListener('load', () => callback(reader.result));
-        reader.readAsDataURL(img);
-    }
-
-    const handleChangeImage = info => {
-        if (info.file.status === 'done') {
-            // Get this url from response in real world.
-            console.log(info)
-            getBase64(info.file.originFileObj, imageUrl => setImage(imageUrl));
-        }
-    };
-
-    const beforeUpload = (file) => {
-        const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-        if (!isJpgOrPng) {
-            console.error('You can only upload JPG/PNG file!');
-        }
-        const isLt2M = file.size / 1024 / 1024 < 2;
-        if (!isLt2M) {
-            console.error('Image must smaller than 2MB!');
-        }
-        return isJpgOrPng && isLt2M;
-    }
-
-    //email validation
-    const onChangeEmail = (event) => {
-        const email = event.target.value;
-        console.log(email)
-        if (emailRegex.test(email)) {
-            setIsValid(true);
-            setMessage('Your email is valid!');
-        } else {
-            setIsValid(false);
-            setMessage('Please enter a valid email!');
-        }
-    }
-
-    //password validation
-    const onChangePassword = (event) => {
-        const password = event.target.value;
-        console.log(password)
-        if (passwordRegex.test(password)) {
-            setIsPasswordValid(true);
-            setMessagePW('Your password is valid!');
-        } else {
-            setIsPasswordValid(false);
-            setMessagePW('Password must be 8 letter');
-        }
-    }
 
 
     return (
@@ -120,21 +75,17 @@ const Signup = () => {
             }}
             onFinish={onFinish}
         >
-            <h1>Signup</h1>
             <Form.Item
-                label="Email"
-                name="email"
+                label="Username"
+                name="username"
                 rules={[
                     {
                         required: true,
-                        message: 'Please input your email!',
+                        message: 'Please input your username!',
                     },
                 ]}
             >
-                <Input onChange={onChangeEmail} />
-                <p className={`message ${isValid ? 'success' : 'error'}`}>
-                    {message}
-                </p>
+                <Input />
             </Form.Item>
 
             <Form.Item
@@ -144,14 +95,10 @@ const Signup = () => {
                     {
                         required: true,
                         message: 'Please input your password!',
-                    }
-                    // { validator: validatePassword }
+                    },
                 ]}
-            >   
-                <Input.Password onChange={onChangePassword}/>
-                <p className={`message ${isPasswordValid ? 'success' : 'error'}`}>
-                    {messagePW}
-                </p>
+            >
+                <Input.Password />
             </Form.Item>
 
             <Form.Item
@@ -180,21 +127,20 @@ const Signup = () => {
                 <Input />
             </Form.Item>
 
-            <Form.Item label="Profile Picture"
-                name="profilePicture">
-                <Upload
-                    name="profilePicture"
-                    listType="picture-card"
-                    beforeUpload={beforeUpload}
-                    showUploadList={false}
-                    action="http://localhost:8000/upload" // change this url after
-
-                    onChange={handleChangeImage}
-                >
-                    {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+            <Form.Item label="Profile Image"
+                name="profileImage"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your profile image!',
+                    },
+                ]}
+            >
+                
+                <Upload {...props}>
+                    <Button icon={<UploadOutlined />}>Product image 1</Button>
                 </Upload>
             </Form.Item>
-
             <Form.Item {...tailLayout}>
                 <Button type="primary" htmlType="submit">
                     Submit
